82635c5ffab0c957c056d5ed92f01fdf470f673a
Artagos <105867005+Artagos@users.noreply.github.com>
2024-08-15 22:27:01 -0400

    Create player.py

---
+++ player/player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, altura, resistencia, salto_maximo, velocidad):
+        self.altura = altura
+        self.resistencia = resistencia
+        self.salto_maximo = salto_maximo
+        self.velocidad = velocidad
+        self.current_resistencia = resistencia
