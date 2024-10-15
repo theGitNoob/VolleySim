a406e89384438e42b0e92ce4ec3acbc6f126e087
Rafael Acosta <theGitNoob@gmail.com>
2024-10-15 12:25:30 -0400

    fix: change starting_params.py team names

--- starting_params.py
+++ starting_params.py
@@ -1,7 +1,7 @@
 from Agents.manager_action_strategy import (ActionRandomStrategy,
                                             ActionSimulateStrategy)
 from Agents.manager_line_up_strategy import LineUpStandardStrategy
-from Agents.player_strategy import MinimaxStrategy, VolleyballStrategy
+from Agents.player_strategy import MinimaxStrategy, VolleyballStrategy, RandomStrategy
 from Simulator.simulation_params import SimulationParams
 
 
@@ -23,116 +23,86 @@ all_random = StartingParams(
     "all_random",
 )
 
-# all_smart = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpFixedStrategy(), LineUpFixedStrategy()),
-#         (ActionSimulateStrategy(), ActionSimulateStrategy()),
-#         (VolleyballStrategy(), VolleyballStrategy())
-#     ),
-#     'all_smart'
-# )
+all_smart = StartingParams(
+    SimulationParams(
+        team_names,
+        (LineUpStandardStrategy(), LineUpStandardStrategy()),
+        (ActionSimulateStrategy(), ActionSimulateStrategy()),
+        (VolleyballStrategy(), VolleyballStrategy())
+    ),
+    'all_smart'
+)
 #
-# smart_line_up = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpFixedStrategy(), LineUpFixedStrategy()),
-#         (ActionRandomStrategy(), ActionRandomStrategy()),
-#         (RandomStrategy(), RandomStrategy())
-#     ),
-#     'smart_line_up'
-# )
+smart_line_up = StartingParams(
+    SimulationParams(
+        team_names,
+        (LineUpStandardStrategy(), LineUpStandardStrategy()),
+        (ActionRandomStrategy(), ActionRandomStrategy()),
+        (RandomStrategy(), RandomStrategy())
+    ),
+    'smart_line_up'
+)
 #
-# smart_vs_random_line_up = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpFixedStrategy(), LineUpRandomStrategy()),
-#         (ActionRandomStrategy(), ActionRandomStrategy()),
-#         (RandomStrategy(), RandomStrategy())
-#     ),
-#     'smart_vs_random_line_up'
-# )
+smart_vs_random_line_up = StartingParams(
+    SimulationParams(
+        team_names,
+        (LineUpStandardStrategy(), LineUpStandardStrategy()),
+        (ActionRandomStrategy(), ActionRandomStrategy()),
+        (RandomStrategy(), RandomStrategy())
+    ),
+    'smart_vs_random_line_up'
+)
 #
-# smart_action = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpRandomStrategy(), LineUpRandomStrategy()),
-#         (ActionSimulateStrategy(), ActionSimulateStrategy()),
-#         (RandomStrategy(), RandomStrategy())
-#     ),
-#     'smart_action'
-# )
-#
-# smart_vs_random_action = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpRandomStrategy(), LineUpRandomStrategy()),
-#         (ActionSimulateStrategy(), ActionRandomStrategy()),
-#         (RandomStrategy(), RandomStrategy())
-#     ),
-#     'smart_vs_random_action'
-# )
-#
-# minimax_vs_minimax_action = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpRandomStrategy(), LineUpRandomStrategy()),
-#         (ActionMiniMaxStrategy(), ActionMiniMaxStrategy()),
-#         (RandomStrategy(), RandomStrategy())
-#     ),
-#     'minimax_vs_minimax_action'
-# )
-#
-# minimax_vs_random_action = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpRandomStrategy(), LineUpRandomStrategy()),
-#         (ActionMiniMaxStrategy(), ActionRandomStrategy()),
-#         (RandomStrategy(), RandomStrategy())
-#     ),
-#     'minimax_vs_random_action'
-# )
-#
-# minimax_vs_smart_action = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpRandomStrategy(), LineUpRandomStrategy()),
-#         (ActionMiniMaxStrategy(), ActionSimulateStrategy()),
-#         (RandomStrategy(), RandomStrategy())
-#     ),
-#     'minimax_vs_smart_action'
-# )
-#
-# smart_player = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpRandomStrategy(), LineUpRandomStrategy()),
-#         (ActionRandomStrategy(), ActionRandomStrategy()),
-#         (VolleyballStrategy(), VolleyballStrategy())
-#     ),
-#     'smart_player'
-# )
-#
-# smart_vs_random_player = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpRandomStrategy(), LineUpRandomStrategy()),
-#         (ActionRandomStrategy(), ActionRandomStrategy()),
-#         (VolleyballStrategy(), RandomStrategy())
-#     ),
-#     'smart_vs_random_player'
-# )
-#
-# minimax_vs_random_player = StartingParams(
-#     SimulationParams(
-#         team_names,
-#         (LineUpRandomStrategy(), LineUpRandomStrategy()),
-#         (ActionRandomStrategy(), ActionRandomStrategy()),
-#         (MinimaxStrategy(), RandomStrategy())
-#     ),
-#     'minimax_vs_random_player'
-# )
-#
+smart_action = StartingParams(
+    SimulationParams(
+        team_names,
+        (LineUpStandardStrategy(), LineUpStandardStrategy()),
+        (ActionSimulateStrategy(), ActionSimulateStrategy()),
+        (RandomStrategy(), RandomStrategy())
+    ),
+    'smart_action'
+)
+
+smart_vs_random_action = StartingParams(
+    SimulationParams(
+        team_names,
+        (LineUpStandardStrategy(), LineUpStandardStrategy()),
+        (ActionSimulateStrategy(), ActionRandomStrategy()),
+        (RandomStrategy(), RandomStrategy())
+    ),
+    'smart_vs_random_action'
+)
+
+smart_player = StartingParams(
+    SimulationParams(
+        team_names,
+        (LineUpStandardStrategy(), LineUpStandardStrategy()),
+        (ActionRandomStrategy(), ActionRandomStrategy()),
+        (VolleyballStrategy(), VolleyballStrategy())
+    ),
+    'smart_player'
+)
+
+smart_vs_random_player = StartingParams(
+    SimulationParams(
+        team_names,
+        (LineUpStandardStrategy(), LineUpStandardStrategy()),
+        (ActionRandomStrategy(), ActionRandomStrategy()),
+        (VolleyballStrategy(), RandomStrategy())
+    ),
+    'smart_vs_random_player'
+)
+
+minimax_vs_random_player = StartingParams(
+    SimulationParams(
+        team_names,
+        (LineUpStandardStrategy(), LineUpStandardStrategy()),
+        (ActionRandomStrategy(), ActionRandomStrategy()),
+        (MinimaxStrategy(), RandomStrategy())
+    ),
+    'minimax_vs_random_player'
+)
+
 minimax_vs_minimax_player = StartingParams(
     SimulationParams(
         team_names,
