97027eea50879531e89612f02a4e5362bc0baba4
Rafael Acosta <theGitNoob@gmail.com>
2024-10-19 18:17:17 -0400

    feat: change simulation size

--- statistics_analysis/save_data.py
+++ statistics_analysis/save_data.py
@@ -13,20 +13,19 @@ class Color:
     GREEN = "\033[32m"
 
 
-df = pd.read_csv('./data/VNL2024Men.csv')
+df = pd.read_csv("./data/VNL2024Men.csv")
 df["Dorsal"] = range(1, len(df) + 1)
 
 params = [
     all_random,
     all_smart,
     smart_line_up,
-    smart_vs_random_line_up,
     smart_action,
     smart_vs_random_action,
     smart_player,
     smart_vs_random_player,
     minimax_vs_random_player,
-    minimax_vs_minimax_player
+    minimax_vs_minimax_player,
 ]
 
 initial_time = time.time()
@@ -37,7 +36,7 @@ for p in params:
     try:
         data = {}
         file_name = f"data/{p.name}.json"
-        for i in range(1):
+        for i in range(10):
             sim = conf_game(p.simulation_params, df)
             s = sim.simulate_and_save()
             data[i] = s
@@ -47,13 +46,19 @@ for p in params:
 
         actual_time = time.time()
 
-        print(Color.GREEN +
-              f'Finished {p.name} simulation in {actual_time - initial_time} seconds' + Color.RESET)
+        print(
+            Color.GREEN
+            + f"Finished {p.name} simulation in {actual_time - initial_time} seconds"
+            + Color.RESET
+        )
         initial_time = actual_time
     except Exception as e:
         actual_time = time.time()
 
         print(
-            Color.RED + f'Error in {p.name} simulation: {e} in {actual_time - initial_time} seconds' + Color.RESET)
+            Color.RED
+            + f"Error in {p.name} simulation: {e} in {actual_time - initial_time} seconds"
+            + Color.RESET
+        )
         initial_time = actual_time
         continue
